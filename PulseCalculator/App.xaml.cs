ba801959aa8e762319f39210a0fd020ebce492de
juny1209 <1209_jun@naver.com>
2025-12-17 17:57:25 +0900

    add source

---
+++ PulseCalculator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PulseCalculator
+{
+	/// <summary>
+	/// App.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ PulseCalculator/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace PulseCalculator
+{
+	public partial class MainWindow
+	{
+		private readonly System.Diagnostics.Stopwatch _stopwatch = new System.Diagnostics.Stopwatch();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			_stopwatch.Start();
+		}
+
+		private void Button1_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			Button1.Content = $"{_stopwatch.ElapsedMilliseconds}ms";
+			_stopwatch.Restart();
+		}
+	}
+}
